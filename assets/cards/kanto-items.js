7e8a866e1077165573a028fa3359ea8d5e2fc7e1
ainotenoue <radio55qq@gmail.com>
2026-07-30 05:34:14 +0900

    Add files via upload

---
+++ assets/cards/kanto-items.js
@@ -0,0 +1,56 @@
+const KANTO_ITEMS = [
+  {
+    "id": "kanto-plain",
+    "name": "関東平野",
+    "category": "plain",
+    "icon": "assets/icons/plain.svg",
+    "x": 52,
+    "y": 43,
+    "side": "left"
+  },
+  {
+    "id": "kanto-mountains",
+    "name": "関東山地",
+    "category": "mountain",
+    "icon": "assets/icons/mountain-area.svg",
+    "x": 27,
+    "y": 43,
+    "side": "left"
+  },
+  {
+    "id": "tone-river",
+    "name": "利根川",
+    "category": "river",
+    "icon": "assets/icons/river.svg",
+    "x": 53,
+    "y": 27,
+    "side": "right"
+  },
+  {
+    "id": "tokyo-bay",
+    "name": "東京湾",
+    "category": "bay",
+    "icon": "assets/icons/bay.svg",
+    "x": 61,
+    "y": 69,
+    "side": "right"
+  },
+  {
+    "id": "boso-peninsula",
+    "name": "房総半島",
+    "category": "peninsula",
+    "icon": "assets/icons/peninsula.svg",
+    "x": 79,
+    "y": 66,
+    "side": "right"
+  },
+  {
+    "id": "miura-peninsula",
+    "name": "三浦半島",
+    "category": "peninsula",
+    "icon": "assets/icons/peninsula.svg",
+    "x": 53,
+    "y": 82,
+    "side": "left"
+  }
+];
