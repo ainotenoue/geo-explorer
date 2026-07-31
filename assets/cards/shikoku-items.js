9a24c5f7c4e9ccaabaae516466827512fddb69c0
ainotenoue <radio55qq@gmail.com>
2026-07-31 15:00:27 +0900

    Add files via upload

---
+++ assets/cards/shikoku-items.js
@@ -0,0 +1,56 @@
+const SHIKOKU_ITEMS = [
+  {
+    "id": "shikoku-mountains",
+    "name": "四国山地",
+    "category": "mountain",
+    "icon": "assets/icons/mountain-area.svg",
+    "x": 54,
+    "y": 52,
+    "side": "left"
+  },
+  {
+    "id": "sanuki-plain",
+    "name": "讃岐平野",
+    "category": "plain",
+    "icon": "assets/icons/plain.svg",
+    "x": 58,
+    "y": 25,
+    "side": "right"
+  },
+  {
+    "id": "yoshino-river",
+    "name": "吉野川",
+    "category": "river",
+    "icon": "assets/icons/river.svg",
+    "x": 58,
+    "y": 42,
+    "side": "left"
+  },
+  {
+    "id": "shimanto-river",
+    "name": "四万十川",
+    "category": "river",
+    "icon": "assets/icons/river.svg",
+    "x": 29,
+    "y": 68,
+    "side": "right"
+  },
+  {
+    "id": "muroto-cape",
+    "name": "室戸岬",
+    "category": "cape",
+    "icon": "assets/icons/cape.svg",
+    "x": 82,
+    "y": 73,
+    "side": "right"
+  },
+  {
+    "id": "ashizuri-cape",
+    "name": "足摺岬",
+    "category": "cape",
+    "icon": "assets/icons/cape.svg",
+    "x": 24,
+    "y": 83,
+    "side": "left"
+  }
+];
