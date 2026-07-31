7d149caa012728cdc806384d5f887f89b707cb81
ainotenoue <radio55qq@gmail.com>
2026-07-31 15:54:57 +0900

    Add files via upload

---
+++ assets/cards/kyushu-okinawa-items.js
@@ -0,0 +1,56 @@
+const KYUSHU_OKINAWA_ITEMS = [
+  {
+    "id": "kyushu-mountains",
+    "name": "九州山地",
+    "category": "mountain",
+    "icon": "assets/icons/mountain-area.svg",
+    "x": 42,
+    "y": 50,
+    "side": "left"
+  },
+  {
+    "id": "chikushi-plain",
+    "name": "筑紫平野",
+    "category": "plain",
+    "icon": "assets/icons/plain.svg",
+    "x": 35,
+    "y": 23,
+    "side": "right"
+  },
+  {
+    "id": "aso-volcano",
+    "name": "阿蘇山",
+    "category": "volcano",
+    "icon": "assets/icons/volcano.svg",
+    "x": 48,
+    "y": 39,
+    "side": "left"
+  },
+  {
+    "id": "chikugo-river",
+    "name": "筑後川",
+    "category": "river",
+    "icon": "assets/icons/river.svg",
+    "x": 37,
+    "y": 31,
+    "side": "right"
+  },
+  {
+    "id": "sakurajima",
+    "name": "桜島",
+    "category": "volcano",
+    "icon": "assets/icons/volcano.svg",
+    "x": 40,
+    "y": 78,
+    "side": "left"
+  },
+  {
+    "id": "okinawa-island",
+    "name": "沖縄島",
+    "category": "island",
+    "icon": "assets/icons/island.svg",
+    "x": 81,
+    "y": 67,
+    "side": "right"
+  }
+];
