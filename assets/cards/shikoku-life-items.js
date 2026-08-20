9169e5e6f7db3acebac562a8b8f2ebd16b9a9268
ainotenoue <radio55qq@gmail.com>
2026-08-21 06:54:21 +0900

    Add files via upload

---
+++ assets/cards/shikoku-life-items.js
@@ -0,0 +1,113 @@
+const SHIKOKU_LIFE_ITEMS = [
+  {
+    id: "07-L-01",
+    name: "ため池",
+    hint: "雨の少ない香川県に多い｜農業用の水をたくわえる",
+    category: "lake",
+    icon: "assets/icons/lake.svg",
+    x: 58,
+    y: 27,
+    side: "left",
+    order: 1
+  },
+  {
+    id: "07-L-02",
+    name: "みかん",
+    hint: "温暖な気候と斜面を生かして栽培｜愛媛県が有名",
+    category: "agriculture",
+    icon: "assets/icons/mikan.svg",
+    x: 27,
+    y: 46,
+    side: "left",
+    order: 2
+  },
+  {
+    id: "07-L-03",
+    name: "促成栽培",
+    hint: "温暖な気候を生かして早く出荷｜高知平野でさかん",
+    category: "agriculture",
+    icon: "assets/icons/agriculture.svg",
+    x: 58,
+    y: 65,
+    side: "left",
+    order: 3
+  },
+  {
+    id: "07-L-04",
+    name: "今治タオル",
+    hint: "愛媛県今治市で発達｜品質の高いタオルで知られる",
+    category: "traditional",
+    icon: "assets/icons/towel.svg",
+    x: 29,
+    y: 28,
+    side: "left",
+    order: 4
+  },
+  {
+    id: "07-L-05",
+    name: "造船",
+    hint: "瀬戸内海沿岸で発達｜愛媛県今治市周辺が代表的",
+    category: "industry",
+    icon: "assets/icons/zosen.svg",
+    x: 31,
+    y: 24,
+    side: "left",
+    order: 5
+  },
+
+  {
+    id: "07-L-06",
+    name: "製紙・パルプ",
+    hint: "愛媛県四国中央市で発達｜紙製品をつくる工業がさかん",
+    category: "industry",
+    icon: "assets/icons/industry.svg",
+    x: 47,
+    y: 31,
+    side: "right",
+    order: 1
+  },
+  {
+    id: "07-L-07",
+    name: "坂出・丸亀工業地域",
+    hint: "香川県の瀬戸内海沿岸に広がる｜工業が集まる地域",
+    category: "industry",
+    icon: "assets/icons/industry.svg",
+    x: 61,
+    y: 24,
+    side: "right",
+    order: 2
+  },
+  {
+    id: "07-L-08",
+    name: "明石海峡大橋",
+    hint: "本州と淡路島を結ぶ｜神戸と四国方面をつなぐ交通の要所",
+    category: "bridge",
+    icon: "assets/icons/bridge.svg",
+    x: 92,
+    y: 21,
+    side: "right",
+    order: 3
+  },
+  {
+    id: "07-L-09",
+    name: "しまなみ海道",
+    hint: "今治と尾道を島づたいに結ぶ｜自転車でも渡れるルート",
+    category: "transport",
+    icon: "assets/icons/shimanami.svg",
+    x: 24,
+    y: 19,
+    side: "right",
+    order: 4
+  },
+  {
+    id: "07-L-10",
+    name: "阿波おどり",
+    hint: "徳島県を代表する夏祭り｜多くの踊り手が町を練り歩く",
+    category: "traditional",
+    icon: "assets/icons/traditional.svg",
+    x: 83,
+    y: 43,
+    side: "right",
+    order: 5
+  }
+];
---
+++ assets/cards/shikoku-nature-items.js
@@ -0,0 +1,113 @@
+const SHIKOKU_NATURE_ITEMS = [
+  {
+    id: "07-N-01",
+    name: "四国山地",
+    hint: "四国の中央部を東西にのびる｜四国の地形の骨格となる山地",
+    category: "mountain",
+    icon: "assets/icons/mountain-area.svg",
+    x: 54,
+    y: 52,
+    side: "left",
+    order: 1
+  },
+  {
+    id: "07-N-02",
+    name: "石鎚山",
+    hint: "愛媛県にある｜西日本で最も高い山",
+    category: "mountain",
+    icon: "assets/icons/mountain.svg",
+    x: 42,
+    y: 43,
+    side: "left",
+    order: 2
+  },
+  {
+    id: "07-N-03",
+    name: "吉野川",
+    hint: "四国山地から徳島県へ流れる｜四国を代表する大きな川",
+    category: "river",
+    icon: "assets/icons/river.svg",
+    x: 65,
+    y: 42,
+    side: "left",
+    order: 3
+  },
+  {
+    id: "07-N-04",
+    name: "四万十川",
+    hint: "高知県西部を流れる｜大きく曲がりながら太平洋へ注ぐ",
+    category: "river",
+    icon: "assets/icons/river.svg",
+    x: 29,
+    y: 68,
+    side: "left",
+    order: 4
+  },
+  {
+    id: "07-N-05",
+    name: "讃岐平野",
+    hint: "香川県に広がる｜雨が少なくため池が多い平野",
+    category: "plain",
+    icon: "assets/icons/plain.svg",
+    x: 58,
+    y: 25,
+    side: "left",
+    order: 5
+  },
+
+  {
+    id: "07-N-06",
+    name: "高知平野",
+    hint: "高知県中央部に広がる｜温暖な気候を生かした農業がさかん",
+    category: "plain",
+    icon: "assets/icons/plain.svg",
+    x: 58,
+    y: 65,
+    side: "right",
+    order: 1
+  },
+  {
+    id: "07-N-07",
+    name: "瀬戸内海",
+    hint: "四国の北側に広がる｜島が多く波がおだやかな海",
+    category: "bay",
+    icon: "assets/icons/bay.svg",
+    x: 49,
+    y: 14,
+    side: "right",
+    order: 2
+  },
+  {
+    id: "07-N-08",
+    name: "室戸岬",
+    hint: "高知県東部の先端｜太平洋へ大きく突き出した岬",
+    category: "cape",
+    icon: "assets/icons/cape.svg",
+    x: 82,
+    y: 73,
+    side: "right",
+    order: 3
+  },
+  {
+    id: "07-N-09",
+    name: "足摺岬",
+    hint: "高知県西南部の先端｜四国南西部を代表する岬",
+    category: "cape",
+    icon: "assets/icons/cape.svg",
+    x: 24,
+    y: 83,
+    side: "right",
+    order: 4
+  },
+  {
+    id: "07-N-10",
+    name: "うず潮",
+    hint: "徳島県と淡路島の間の鳴門海峡｜潮の流れで大きな渦ができる",
+    category: "sea",
+    icon: "assets/icons/uzushio.svg",
+    x: 91,
+    y: 30,
+    side: "right",
+    order: 5
+  }
+];
